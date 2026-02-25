d866e16a7ab1faee9897f664ef585dc45d985094
Kajzawesome <kaj.jacobs@gmail.com>
2026-02-25 10:01:48 -0700

    Updated project name & got basic routes added

--- Mission #8 - Group 8/Controllers/HomeController.cs
+++ Mission #8 - Group 8/Controllers/HomeController.cs
@@ -8,12 +8,75 @@ namespace Mission__8___Group_8.Controllers
     {
         public IActionResult Index()
         {
+            // task quadrant display?
+            /* var tasks1 = _context.Tasks.Where(CategoryId = 1).ToList();
+             * var tasks2 = _context.Tasks.Where(CategoryId = 2).ToList();
+             * var tasks3 = _context.Tasks.Where(CategoryId = 3).ToList();
+             * var tasks4 = _context.Tasks.Where(CategoryId = 4).ToList();
+             */
             return View();
         }
 
-        public IActionResult Privacy()
+        // GET: Home/Create 
+        public IActionResult Create(Task response)
         {
+            if (ModelState.IsValid)
+            {
+                _context.Tasks.Add(response);
+                _context.SaveChanges();
+                return View("Confirmation", response);
+            }
+
+            ViewBag.Categories = new SelectList(_context.Categories.ToList(), "CategoryId", "Category");
+            return View(response);
+        }
+
+        // GET: Home/Confirmation for Create and Update
+        public IActionResult Confirmation()
+        {
+            return View(); 
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var task = _context.Tasks.Find(id);
+
+            if (task == null) {
+                return NotFound();
+            }
+
+            ViewBag.Categories = new SelectList(_context.Categories.ToList(), "CategoryId", "CategoryName", task.CategoryId);
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Task response)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Tasks.Update(response);
+                _context.SaveChanges();
+                return View("Confirmation", response);
+            }
+            ViewBag.Categories = new SelectList(_context.Categories.ToList(), "CategoryId", "Category");
+            return View(response);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id) {
+            var task = _context.Tasks.Find(id);
+            if (task == null) {
+                return NotFound();
+            }
+            return View(task);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Task task) {
+            _context.Tasks.Remove(task);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
